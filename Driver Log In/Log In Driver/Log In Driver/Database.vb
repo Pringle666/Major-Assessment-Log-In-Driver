c74070507f222193ead69ba0726c62be1825fea0
Pringle666 <jarrad.pringle@education.nsw.gov.au>
2016-03-28 20:39:22 +1100

    Added files via upload

---
+++ Driver Log In/Log In Driver/Log In Driver/Database.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Data.OleDb
+Module Database
+    Public connection As OleDbConnection
+
+    Sub connecttodatabase()
+        'connect the connection string
+        connection = New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Jarra\Desktop\School Yr12\SDD\Student Database\Driver Log In\Log In Driver\Log In Driver\Student.accdb;")
+
+        'open the connection
+        connection.Open()
+
+    End Sub
+
+    Sub disconnectfromdatabase()
+        'close the connection
+        connection = Nothing
+    End Sub
+
+    Sub ViewUsers()
+        ''build the query
+        'Dim q As String = "SELECT * FROM Username"
+
+        ''execute the query
+        'Dim cmd As New OleDbCommand(q, connection)
+        'Dim reader As OleDbDataReader = cmd.ExecuteReader
+
+
+
+        'reader.Close()
+    End Sub
+End Module
---
+++ Driver Log In/Log In Driver/Log In Driver/MainWindow.xaml.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Data.OleDb
+
+Class MainWindow
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+        connecttodatabase()
+    End Sub
+
+    Private Sub Window_Closing(sender As Object, e As ComponentModel.CancelEventArgs)
+        disconnectfromdatabase()
+    End Sub
+
+    Private Sub BtnLogIn_Click(sender As Object, e As RoutedEventArgs) Handles BtnLogIn.Click
+        'Try
+        'build the query
+        Dim q As String = "SELECT * FROM User"
+
+            'execute the query
+            Dim cmd As New OleDbCommand(q, connection)
+            Dim reader As OleDbDataReader = cmd.ExecuteReader
+            Dim username As String
+            Dim password As String
+
+            While reader.Read
+                username = reader("Username")
+                password = reader("Password")
+                If TxtbxUsername.Text = username & TXTbxPassword.Password = password Then
+                    MsgBox("working")
+                End If
+            End While
+
+            reader.Close()
+            'Catch x As OleDbException
+        '   TxtDisplay.Text = x.ToString
+        'End Try
+    End Sub
+End Class
